dc4b6e0d69de2116cd296e7cb2854a3cfd7017b2
Aditya Jadhav <adityadjadhav17@gmail.com>
2020-08-08 15:39:55 +0530

    Committed changes

--- Single Number.py
+++ Single Number.py
@@ -3,9 +3,13 @@
 function returns the only unique number present in the nums list
 '''
 
+#Problem Statement: a list contains all number two times except one find the unique number
+'''
+function returns the only unique number present in the nums list
+'''
+
 def singleNumber(self, nums: List[int]) -> int:
     num = nums[0]
     for i in nums[1:]:
          num = num^i
     return num
-        
--- singularity.py
+++ singularity.py
@@ -1,4 +1,4 @@
-'''author: shriaas2898
+"""'''author: shriaas2898
    description: Python file contains function to calculate determinant of matrix
    and a  function to check if the matrix is singular'''
 # for calculating determinant of a matrix
@@ -49,4 +49,63 @@ def build_matrix(file_name):
 
         return matrix
 if __name__ == '__main__':
-    
+    """
+
+''' Modified Code'''
+global N 
+N = 3
+  
+# Function to get cofactor of mat[p][q] in temp[][]. 
+# n is current dimension of mat[][] 
+def getCofactor(mat,temp,p,q,n): 
+    i = 0
+    j = 0
+      
+    # Looping for each element of the matrix 
+    for row in range(n): 
+        for col in range(n): 
+              
+            # Copying into temporary matrix only  
+            # those element which are not in given  
+            # row and column 
+            if (row != p and col != q): 
+                temp[i][j] = mat[row][col] 
+                j += 1
+                  
+                # Row is filled, so increase row 
+                # index and reset col index 
+                if (j == n - 1): 
+                    j = 0
+                    i += 1
+  
+# Recursive function to check if mat[][] is 
+# singular or not. */ 
+def isSingular(mat,n): 
+    D = 0 # Initialize result 
+      
+    # Base case : if matrix contains single element 
+    if (n == 1): 
+        return mat[0][0] 
+          
+    temp = [[0 for i in range(N + 1)] for i in range(N + 1)]# To store cofactors 
+      
+    sign = 1 # To store sign multiplier 
+  
+    # Iterate for each element of first row 
+    for f in range(n): 
+          
+        # Getting Cofactor of mat[0][f] 
+        getCofactor(mat, temp, 0, f, n) 
+        D += sign * mat[0][f] * isSingular(temp, n - 1) 
+          
+        # terms are to be added with alternate sign 
+        sign = -sign 
+    return D 
+  
+# Driver program to test above functions 
+if __name__ == '__main__': 
+    mat = [[4, 10, 1],[0, 0, 0],[1, 4, -3]] 
+    if (isSingular(mat, N)): 
+        print("Matrix is Singular") 
+    else: 
+        print("Matrix is non-singular")     
